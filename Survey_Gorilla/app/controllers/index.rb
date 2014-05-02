ba1186987485fff2c4d089eecaca5b1ccb93698f
& Jaimin Patel <pair+gpuglia.jaimin2663@devbootcamp.com>
2014-05-02 15:56:46 -0500

    Adding bcrypt

--- Survey_Gorilla/app/controllers/index.rb
+++ Survey_Gorilla/app/controllers/index.rb
@@ -1,4 +1,26 @@
+enable "sessions"
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+post '/login' do
+  User.authenticate(username, password)
+end
+
+post '/register' do
+  username = params[:username]
+  pass = params[:password]
+
+  if User.find_by_username(username)
+    @error = true
+    erb :index
+  else
+    User.create!(username: username, password: pass)
+    this_user = User.find_by_username(username)
+    this_user.password = pass
+    this_user.save
+    erb :dashboard
+  end
 end
